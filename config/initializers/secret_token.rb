0e88cd96ca557973ce137bf973202181220ac389
Simon Crabtree <simon@Simon-Crabtrees-MacBook-Pro.local>
2011-11-19 12:48:02 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SiBlog::Application.config.secret_token = 'fbd2c2883678d66590c98bf0a2ec9a87b957982edfd1638bd9498ee7649d56a2802813c13b149d033d4164af0a157811891b34980182400b37faa335619bba08'
